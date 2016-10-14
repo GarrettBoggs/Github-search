8b679a79ba0091fde598e85d75d987451fea94ed
Garrett Boggs <garrett.boggs@my.wheaton.edu>
2016-10-14 08:40:47 -0700

    display repo name and repo description

--- js/github-interface.js
+++ js/github-interface.js
@@ -4,8 +4,9 @@ var Search = require('./../js/search.js').searchModule;
 $(document).ready(function(){
     var testSearch = new Search();
      $("form").submit(function(event){
-          var user = $("#user").val();
           event.preventDefault();
+          $("#output").empty();
+          var user = $("#user").val();
           testSearch.getRepos(user);
        });
     });
--- js/search.js
+++ js/search.js
@@ -7,6 +7,11 @@ function Search(){
  Search.prototype.getRepos = function(user){
   $.get('https://api.github.com/users/' + user + '/repos?access_token=' + apiKey).then(function(response){
     console.log(response);
+    for(var i = 0; i < response.length ; i++)
+    {
+      $("#output").append("<li>" + response[i].full_name + "</li>");
+      $("#output").append("<li>" + response[i].description + "</li>");
+    }
   }).fail(function(error){
     console.log(error.responseJSON.message);
   });
