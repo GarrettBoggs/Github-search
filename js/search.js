f4b1ab88680428c33bfd4be5bd3339ce2c7ee9a4
Garrett Boggs <garrett.boggs@my.wheaton.edu>
2016-10-14 08:27:21 -0700

    use API to access REPO data rather than user data

--- js/search.js
+++ js/search.js
@@ -5,7 +5,7 @@ function Search(){
 }
 
  Search.prototype.getRepos = function(user){
-  $.get('https://api.github.com/users/' + user + '?access_token=' + apiKey).then(function(response){
+  $.get('https://api.github.com/users/' + user + '/repos?access_token=' + apiKey).then(function(response){
     console.log(response);
   }).fail(function(error){
     console.log(error.responseJSON.message);
