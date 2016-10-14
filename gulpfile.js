86547b75f94946487d798b761e63770d3782026f
Garrett Boggs <garrett.boggs@my.wheaton.edu>
2016-10-14 10:15:46 -0700

    add bower, clean, and concatInterface to gulpfile

--- gulpfile.js
+++ gulpfile.js
@@ -43,3 +43,15 @@ gulp.task('bowerCSS', function () {
     .pipe(concat('vendor.css'))
     .pipe(gulp.dest('./build/css'));
 });
+
+gulp.task('bower', ['bowerJS', 'bowerCSS']);
+
+gulp.task("clean", function(){
+  return del(['build', 'tmp']);
+});
+
+gulp.task('concatInterface', function() {
+  return gulp.src(['./js/*-interface.js'])
+    .pipe(concat('allConcat.js'))
+    .pipe(gulp.dest('./tmp'));
+});
