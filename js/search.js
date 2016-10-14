3744f388bd0cf3b44a7ec8b18f75fcdfadaa8c68
Garrett Boggs <garrett.boggs@my.wheaton.edu>
2016-10-14 09:11:40 -0700

    add error message when user not found

--- js/search.js
+++ js/search.js
@@ -7,14 +7,20 @@ function Search(){
  Search.prototype.getRepos = function(user){
   $.get('https://api.github.com/users/' + user + '/repos?access_token=' + apiKey).then(function(response){
     console.log(response);
-    for(var i = 0; i < response.length ; i++)
-    {
-      $("#output").append("<li> Name: " + response[i].full_name + "</li>");
-      $("#output").append("<li> Description: " + response[i].description + "</li>");
-      $("#output").append("<li> Date Created: " + moment(response[i].created_at).format("YYYY-MM-DD") + "</li>");
+    if(response.length == 0){
+      $("#output").append("<li>  No User History Found! Try Again? </li>");
+    }
+    else{
+      for(var i = 0; i < response.length ; i++)
+      {
+        $("#output").append("<li> Name: " + response[i].full_name + "</li>");
+        $("#output").append("<li> Description: " + response[i].description + "</li>");
+        $("#output").append("<li> Date Created: " + moment(response[i].created_at).format("YYYY-MM-DD") + "</li>");
+      }
     }
   }).fail(function(error){
     console.log(error.responseJSON.message);
+    $("#output").append("<li>  No User History Found! Try Again? </li>");
   });
 };
 
